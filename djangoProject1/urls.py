ae5e49c5ce53e0a779f0817049a3f88005e2d386
Mahsima <mahsima1884@gmail.com>
2024-09-13 03:10:27 +0330

    commit first changes

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from product.views import product_list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', product_list_view)
+
 ]
